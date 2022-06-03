0e4e66354aadab751bd829648632208c52297f85
SebaChuaqui <schuaquicano@gmail.com>
2022-06-02 22:04:48 -0300

    Estudio XVII

--- getdata/index.js
+++ getdata/index.js
@@ -7,3 +7,17 @@ getData();
 fetch('https://swapi.dev/api/people/')
 .then(response => response.json())
 .then(result => console.log(result))
+
+
+const json = '{"edad" : 30, "nombre" : "Jocelyn"}';
+
+try {
+    const usuario = JSON.parse(json);
+    console.log(usuario.edad);
+    console.log(usuario.nombres);
+} catch (error) {
+    console.log('Error')
+}
+
+const z = {y:20,z:30};
+z.y();
